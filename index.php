0f2bbf724360767865cb5ff1252b86dd3ff8b7eb
Niran Lohmaneeratana <niran.lohmaneeratana@tremontui.com>
2016-03-13 16:47:22 -0400

    Initial Setup for Tremontui API

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * BOOTSTRAP
+ */
+require_once './vendor/autoload.php';
+
+session_start();
+
+/**
+ *	NAMESPACING
+ */
+use Slim\Views\PhpRenderer;	
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$configuration = [
+	'settings' => [
+		'displayErrorDetails' => true,
+	],
+];
+$c = new \Slim\Container( $configuration );
+
+$app = new Slim\App( $c );
+
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer( './templates' );
+
+/**
+ *	MIDDLEWARE
+ */
+$app->add( function( Request $request, Response $response, callable $next ) {
+    $uri = $request->getUri();
+    $path = $uri->getPath();
+    if ($path != '/' && substr($path, -1) == '/') {
+        // permanently redirect paths with a trailing slash
+        // to their non-trailing counterpart
+        $uri = $uri->withPath(substr($path, 0, -1));
+        return $response->withRedirect((string)$uri, 301);
+    }
+
+    return $next($request, $response);
+});
+
+$app->get( '/', function( $request, $response, $args ){
+	
+	$onload = ['title' => 'Tremont UI API Documentation'];
+	
+	return $this->renderer->render( $response, '/docs.php', $onload );
+	
+});
+
+$app->run();
+?>
