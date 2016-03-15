884c97f859e79e124f75bcc17a81a2c9c832b99d
Niran Lohmaneeratana <niran.lohmaneeratana@tremontui.com>
2016-03-14 22:40:19 -0400

    Moved config to Config folder.

--- api_bootstrap.php
+++ api_bootstrap.php
@@ -4,6 +4,6 @@ require_once( './autoload.php' );
 
 DEFINE( "SUPER_ROOT", $_SERVER['DOCUMENT_ROOT'] . '/../' );
 
-$db_ini = parse_ini_file( SUPER_ROOT . 'db_config.ini' );
+$db_ini = parse_ini_file( SUPER_ROOT . 'config/db_config.ini' );
 
 ?>
